d2fb50543914b66369bd814481350161286e214c
Athila <athilaca26@gmail.com>
2024-03-22 12:11:47 +0530

    414th problem solved

---
+++ 414. Third Maximum Number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        a=set(nums)
+        b=list(a)
+        b.sort(reverse=True)
+
+        if len(b)>=3:
+          return b[2] 
+        else:
+            return max(b) 
